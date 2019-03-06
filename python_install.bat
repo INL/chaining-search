296bd43da729d2a858999004d446f23b1d40280f
Mathieu Fannee <mathieu.fannee@inl.nl>
2019-03-06 16:50:49 +0100

    Split install.bat in install.bat and python_install.bat, to solve installations issues

---
+++ python_install.bat
@@ -0,0 +1,9 @@
+echo Download and install python
+
+powershell -command "& { [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 ; (New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.7.2/python-3.7.2-amd64.exe', 'python-3.7.2-amd64.exe') }"
+
+python-3.7.2-amd64.exe 
+
+echo Set environment variable 
+
+powershell -command "& { [Environment]::SetEnvironmentVariable('Path', $env:USERPROFILE+'\AppData\Local\Programs\Python\Python37\;'+$env:USERPROFILE+'\AppData\Local\Programs\Python\Python37\Scripts\;'+$env:Path, 'User') }"
