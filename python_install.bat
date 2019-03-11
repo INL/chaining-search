765be74cfb057a884f4834bbf11b3f2135528382
Mathieu Fannee <mathieu.fannee@inl.nl>
2019-03-11 11:37:41 +0100

    Made sure that Python installer closes the cmd prompt window when installation is finished.

--- python_install.bat
+++ python_install.bat
@@ -7,3 +7,5 @@ python-3.7.2-amd64.exe
 echo Set environment variable 
 
 powershell -command "& { [Environment]::SetEnvironmentVariable('Path', $env:USERPROFILE+'\AppData\Local\Programs\Python\Python37\;'+$env:USERPROFILE+'\AppData\Local\Programs\Python\Python37\Scripts\;'+$env:Path, 'User') }"
+
+exit
